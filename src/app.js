8af7400baf9d612180065b85864dbaff667c0fcb
ryrxin <ryrxin@gmail.com>
2024-06-22 17:26:49 +0800

    removed images and removed cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 // INCLUDES
 const express = require("express");
-// const bodyParser = require("body-parser");
-const cors = require("cors");
 
 // CREATE APP
 const app = express();
 
 // USES
-// app.use(bodyParser.json());
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
